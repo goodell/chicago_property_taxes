8de9ea5e4b16afd727f9deac18d606a0f3f966e7
Dave Goodell <dave@goodell.io>
2020-06-27 16:30:10 -0700

    much improved queries, plus two csv result dumps

--- possible_coach_houses.sql
+++ possible_coach_houses.sql
@@ -3,22 +3,61 @@
 -- https://datacatalog.cookcountyil.gov/Property-Taxation/Cook-County-Assessor-s-Residential-Property-Charac/bcnq-qi2z
 -- https://twitter.com/stevevance/status/1275152464635985922
 
-with repeated_pins as
+-- example PIN that seems to have a coach house (from Google Maps satellite and street view):
+-- https://www.cookcountyassessor.com/pin/19181230650000
+--
+-- very small differences appear to be typos/errors
+-- e.g., https://www.cookcountyassessor.com/pin/19034090090000 has rows with bldg_sf={1225,1226}
+--
+-- TODO should there be any filter on the high end, e.g. for https://www.cookcountyassessor.com/pin/01131010080000 ?
+
+with
+unique_pin_sf as
 (
-    select distinct tax_year, pin, bldg_sf, count(*) as num_sharing_pin 
-	from chi_taxes."characteristics" c
-	where tax_year = '2019'
-		and class like '2%'
-		-- 295 is "Individually-owned townhome or row house up to 62 years of age", seems to not be what we want
-		-- example: https://www.cookcountyassessor.com/pin/01011110100000
-		and class != '295'
-	group by tax_year, pin, bldg_sf
-	having count(*) > 1
-	order by pin, tax_year
+    select distinct tax_year, pin, bldg_sf
+    from chi_taxes.unique_characteristics
+    where tax_year = '2019'
+        and class like '2%'
+        -- excluded property classes:
+        -- 211 is "Apartment building with 2 to 6 units, any age"
+        --
+        -- 212 is "Mixed-use commercial/residential building with apartment and commercial area
+        -- totaling 6 units or less with a square foot area less than 20,000 square feet, any age"
+        --
+        -- 295 is "Individually-owned townhome or row house up to 62 years of age"
+        -- example: https://www.cookcountyassessor.com/pin/01011110100000
+        and class not in ('211','212','295')
+        and bldg_sf != ''
+),
+repeated_pins as
+(
+    select
+        tax_year
+        , pin
+        , min(cast(bldg_sf as bigint)) as min_bldg_sf
+        , max(cast(bldg_sf as bigint)) as max_bldg_sf
+        , round(1.0 * max(cast(bldg_sf as bigint)) / min(cast(bldg_sf as bigint)), 4) as bldg_sf_ratio
+        , count(*) as num_sharing_pin
+    from unique_pin_sf
+    group by tax_year, pin
+    -- count=1 have only a single bldg_sf value for the PIN
+    -- count > 2 seem to be apartments and row houses without specifically being classed as such (e.g., 17293090540000)
+    having count(*) = 2
+    order by pin, tax_year
 )
 
--- select count(*)
-select c2.pin, c2.bldg_sf, c2.class, c2.multi_code, c2.multi_ind, c2.multi_family_ind, c2.*
-from chi_taxes."characteristics" c2 
+-- dump possible coach house PINs, together with min/max/ratio info
+select *
+from repeated_pins
+order by bldg_sf_ratio
+
+/*
+-- a join to emit all (previously deduped) rows+columns for possible coach house PINs
+select uc.pin, uc.bldg_sf, uc.class, uc.multi_code, uc.multi_ind, uc.multi_family_ind, uc.*
+from chi_taxes.unique_characteristics uc
 inner join repeated_pins
-on c2.pin = repeated_pins.pin and c2.tax_year = repeated_pins.tax_year
+on uc.pin = repeated_pins.pin
+    and uc.tax_year = repeated_pins.tax_year
+    and uc.bldg_sf = repeated_pins.min_bldg_sf
+order by uc.pin
+*/
--- possible_coach_houses_pins_only.sql
+++ possible_coach_houses_pins_only.sql
@@ -3,21 +3,44 @@
 -- https://datacatalog.cookcountyil.gov/Property-Taxation/Cook-County-Assessor-s-Residential-Property-Charac/bcnq-qi2z
 -- https://twitter.com/stevevance/status/1275152464635985922
 
-with repeated_pins as
+-- example PIN that seems to have a coach house (from Google Maps satellite and street view):
+-- https://www.cookcountyassessor.com/pin/19181230650000
+
+with
+unique_pin_sf as
 (
-    select distinct tax_year, pin, bldg_sf, count(*) as num_sharing_pin 
-	from chi_taxes."characteristics" c
-	where tax_year = '2019'
-		and class like '2%'
-		-- 295 is "Individually-owned townhome or row house up to 62 years of age", seems to not be what we want
-		-- example: https://www.cookcountyassessor.com/pin/01011110100000
-		and class != '295'
-	group by tax_year, pin, bldg_sf
-	having count(*) > 1
-	order by pin, tax_year
+    select distinct tax_year, pin, bldg_sf
+    from chi_taxes.unique_characteristics
+    where tax_year = '2019'
+        and class like '2%'
+        -- excluded property classes:
+        -- 211 is "Apartment building with 2 to 6 units, any age"
+        --
+        -- 212 is "Mixed-use commercial/residential building with apartment and commercial area
+        -- totaling 6 units or less with a square foot area less than 20,000 square feet, any age"
+        --
+        -- 295 is "Individually-owned townhome or row house up to 62 years of age"
+        -- example: https://www.cookcountyassessor.com/pin/01011110100000
+        and class not in ('211','212','295')
+        and bldg_sf != ''
+),
+repeated_pins as
+(
+    select
+        tax_year
+        , pin
+        , min(cast(bldg_sf as bigint)) as min_bldg_sf
+        , max(cast(bldg_sf as bigint)) as max_bldg_sf
+        , round(1.0 * max(cast(bldg_sf as bigint)) / min(cast(bldg_sf as bigint)), 4) as bldg_sf_ratio
+        , count(*) as num_sharing_pin
+    from unique_pin_sf
+    group by tax_year, pin
+    -- count=1 have only a single bldg_sf value for the PIN
+    -- count > 2 seem to be apartments and row houses without specifically being classed as such (e.g., 17293090540000)
+    having count(*) = 2
+    order by pin, tax_year
 )
 
--- select count(*)
 select distinct pin
 from repeated_pins
 order by pin
